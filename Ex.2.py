a76b3586441c631b98b2c528b9fd3f79e72f68fd
Satrirat Seewan <115156411+satrirattt@users.noreply.github.com>
2023-08-16 16:42:29 +0700

    Create Ex.2.py

---
+++ Ex.2.py
@@ -0,0 +1,21 @@
+print('>> Program Find Maximum Digit <<')
+num1 = int(input('Enter number 1 : '))
+num2 = int(input('Enter number 2 : '))
+num3 = int(input('Enter number 3 : '))
+num4 = int(input('Enter number 4 : '))
+num5 = int(input('Enter number 5 : '))
+Max = 0
+if Max < num1:
+    Max = num1
+if Max < num2:
+    Max = num2
+if Max < num3:
+    Max = num3
+if Max < num4:
+    Max = num4
+if Max < num5:
+    Max = num5
+    
+
+print(f"Your enter number {num1}, {num2}, {num3}, {num4}, {num5}")
+print(f"Maximum number is {Max}")
