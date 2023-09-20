cef3c6f2bf9f5810d74038290b3d6f914b687910
Satrirat Seewan <115156411+satrirattt@users.noreply.github.com>
2023-09-20 16:12:35 +0700

    Add files via upload

---
+++ hello_pgz.py
@@ -0,0 +1,11 @@
+import pgzrun
+
+def draw():
+    screen.fill(('black'))
+    screen.draw.text("Hello World" , topleft=(10,10),fontsize=30)
+
+TITIE = "Hello World By Pygame Zero"
+WIDTH = 400
+HEIGHT = 300
+
+pgzrun.go()
