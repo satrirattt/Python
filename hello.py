dabef4aa6a47b4dbb824c8382b8411f634743466
Satrirat Seewan <115156411+satrirattt@users.noreply.github.com>
2024-07-03 15:17:23 +0700

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,13 @@
+print("Python")
+print("Hello world.")
+print(100)
+print('*******')
+print('******')
+print('*****')
+print('****')
+print('***')
+print('**')
+print('*')
+a=float(input('*******'))
+a=a-1
+
